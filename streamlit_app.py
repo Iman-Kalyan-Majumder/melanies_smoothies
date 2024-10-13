7042adfeffd8f0abf2a343097b9eb7cde546b485
Iman Kalyan Majumder <77956318+Iman-Kalyan-Majumder@users.noreply.github.com>
2024-10-13 19:51:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,27 +9,30 @@ st.write(
     """
 )
 
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name on your Smoothie will be: ', name_on_order)
+
 cnx = st.connection("snowflake")
 session = cnx.session()
-
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     "Choose upto five ingredients:",
-    my_dataframe
+    my_dataframe,
+    max_selections=5
 )
 
 ingredients = ''
 for ingredient in ingredients_list:
     ingredients+=ingredient+' '
 
-my_insert_stmt = 'insert into smoothies.public.ORDERS (ingredients) values (\''+ingredients+'\');'
+my_insert_stmt = 'insert into smoothies.public.ORDERS (ingredients, name_on_order) values (\''+ingredients+'\',\''+name_on_order+'\');'
 time_to_insert = st.button('Submit Order')
 
 if time_to_insert:
     if ingredients:
         session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered!', icon='✅')
+        st.success('Your Smoothie is ordered, '+name_on_order+'!', icon='✅')
     else:
         st.write('Please select some ingredients!')
